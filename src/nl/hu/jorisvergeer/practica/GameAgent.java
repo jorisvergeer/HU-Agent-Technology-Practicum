1127727358817794fc80de1923cb02d0258f1b92
Joris Vergeer <joris@joris-HP-Pavilion-g7-Notebook-PC.(none)>
2013-04-16 15:39:01 +0200

    SeriallVersions in Agent class

--- src/nl/hu/jorisvergeer/practica/GameAgent.java
+++ src/nl/hu/jorisvergeer/practica/GameAgent.java
@@ -4,9 +4,12 @@ import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 
 public class GameAgent extends Agent {
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void setup() {
 		addBehaviour(new CyclicBehaviour() {
+			private static final long serialVersionUID = 1L;
 
 			@Override
 			public void action() {
